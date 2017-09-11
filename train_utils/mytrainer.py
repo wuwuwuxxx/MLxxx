8c86b1518bc7e20c6b7e65d0e0d02004fa33668b
wuwuwuxxx <wuwuwuxxx@gmail.com>
2017-09-11 16:13:09 +0800

    auto run fold validation

--- train_utils/mytrainer.py
+++ train_utils/mytrainer.py
@@ -43,29 +43,33 @@ class MyTrainer(mx.gluon.Trainer):
         smoothing_constant = 0.01
         e = 0
         moving_loss = None
-        while True:
-            for i, (data, label) in enumerate(self._train_data_iter):
-                data = data.as_in_context(self._ctx)
-                label = label.as_in_context(self._ctx)
-                with autograd.record():
-                    output = self._net(data)
-                    loss = self._loss(output, label)
-                loss.backward()
-                self.step(data.shape[0])
+        try:
+            while True:
+                for i, (data, label) in enumerate(self._train_data_iter):
+                    data = data.as_in_context(self._ctx)
+                    label = label.as_in_context(self._ctx)
+                    with autograd.record():
+                        output = self._net(data)
+                        loss = self._loss(output, label)
+                    loss.backward()
+                    self.step(data.shape[0])
 
-                ##########################
-                #  Keep a moving average of the losses
-                ##########################
-                curr_loss = nd.mean(loss).asscalar()
-                moving_loss = (curr_loss if moving_loss is None else (1 - smoothing_constant) * moving_loss +
-                                                                     (smoothing_constant) * curr_loss)
-            if e % self._do_ckpt_epochs == 0:
-                test_accuracy = self.evaluate_accuracy(self._val_data_iter)
-                print("Epoch {}. Loss: {}, Test_acc {}, Learning rate: {}".format(
-                    e, moving_loss, test_accuracy, self.learning_rate))
-            else:
-                print("Epoch %s. Loss: %s" % (e, moving_loss))
-            e += 1
-            if self.learning_rate < 1e-7:
-                break
-        print('best validation accuracy is {}'.format(self._best_acc))
+                    ##########################
+                    #  Keep a moving average of the losses
+                    ##########################
+                    curr_loss = nd.mean(loss).asscalar()
+                    moving_loss = (curr_loss if moving_loss is None else (1 - smoothing_constant) * moving_loss +
+                                                                         (smoothing_constant) * curr_loss)
+                if e % self._do_ckpt_epochs == 0:
+                    test_accuracy = self.evaluate_accuracy(self._val_data_iter)
+                    print("Epoch {}. Loss: {}, Test_acc {}, Learning rate: {}".format(
+                        e, moving_loss, test_accuracy, self.learning_rate))
+                else:
+                    print("Epoch %s. Loss: %s" % (e, moving_loss))
+                e += 1
+                if self.learning_rate < 1e-7:
+                    break
+        except KeyboardInterrupt:
+            print('best validation accuracy is {}'.format(self._best_acc))
+        except Exception:
+            raise
--- v1/train.py
+++ v1/train.py
@@ -13,64 +13,66 @@ from train_utils.mytrainer import MyTrainer
 root_path = '/media/xxx/Data/isprs/vaihingen'
 datalist = os.listdir(os.path.join(root_path, 'gts_for_participants'))
 k = 5
-mask = [True] * 5
+mask = [True] * k
 k_fold_idx = divide(list(range(len(datalist))), k=k)
-val_idx = 0
-mask[val_idx] = False
-assert val_idx < k
-val_idx = k_fold_idx[val_idx]
-val_idx = val_idx.astype(np.int32)
-k_fold_idx = k_fold_idx[mask]
-train_idx = np.array([])
-for fold in k_fold_idx:
-    train_idx = np.append(train_idx, fold)
-train_idx = train_idx.astype(np.int32)
-# read images
-if not os.path.exists('../images.npy'):
-    paths = [os.path.join(root_path, 'top', name) for name in datalist]
-    images = [tif.imread(p) for p in paths]
-    np.save('../images.npy', images)
-else:
-    images = np.load('../images.npy')
-# read labels
-if not os.path.exists('../labels.npy'):
-    paths = glob.glob(os.path.join(root_path, 'gts_for_participants', '*.tif'))
-    labels = [tif.imread(p) for p in paths]
-    l = list()
-    for label in labels:
-        h, w, _ = label.shape
-        t = np.zeros((h, w), dtype=np.uint8)
-        t[np.all(label == [0, 0, 255], axis=-1)] = 1
-        t[np.all(label == [0, 255, 255], axis=-1)] = 2
-        t[np.all(label == [0, 255, 0], axis=-1)] = 3
-        t[np.all(label == [255, 255, 0], axis=-1)] = 4
-        t[np.all(label == [255, 0, 0], axis=-1)] = 5
-        l.append(t)
-    np.save('../labels.npy', l)
-else:
-    labels = np.load('../labels.npy')
 
-ctx = mx.gpu()
-step = 32 * 8
-train_data = mx.gluon.data.DataLoader(Isprs(images[train_idx], labels[train_idx], step, training=True),
-                                      batch_size=min(24, len(train_idx)), shuffle=True,
-                                      last_batch='rollover')
-val_data = mx.gluon.data.DataLoader(Isprs(images[val_idx], labels[val_idx], step), batch_size=len(val_idx),
-                                    last_batch='keep')
-net = Unet(6)
-ckpt_name = 'ckpt'
-if os.path.exists(ckpt_name):
-    net.load_params(ckpt_name, ctx=ctx, allow_missing=False)
-    print('load params from ckpt')
-else:
-    net.collect_params().initialize(init=mx.init.MSRAPrelu('in'), ctx=ctx)
+for v in range(k):
+    val_idx = v
+    mask[val_idx] = False
+    assert val_idx < k
+    val_idx = k_fold_idx[val_idx]
+    val_idx = val_idx.astype(np.int32)
+    k_fold_idx = k_fold_idx[mask]
+    train_idx = np.array([])
+    for fold in k_fold_idx:
+        train_idx = np.append(train_idx, fold)
+    train_idx = train_idx.astype(np.int32)
+    # read images
+    if not os.path.exists('../images.npy'):
+        paths = [os.path.join(root_path, 'top', name) for name in datalist]
+        images = [tif.imread(p) for p in paths]
+        np.save('../images.npy', images)
+    else:
+        images = np.load('../images.npy')
+    # read labels
+    if not os.path.exists('../labels.npy'):
+        paths = glob.glob(os.path.join(root_path, 'gts_for_participants', '*.tif'))
+        labels = [tif.imread(p) for p in paths]
+        l = list()
+        for label in labels:
+            h, w, _ = label.shape
+            t = np.zeros((h, w), dtype=np.uint8)
+            t[np.all(label == [0, 0, 255], axis=-1)] = 1
+            t[np.all(label == [0, 255, 255], axis=-1)] = 2
+            t[np.all(label == [0, 255, 0], axis=-1)] = 3
+            t[np.all(label == [255, 255, 0], axis=-1)] = 4
+            t[np.all(label == [255, 0, 0], axis=-1)] = 5
+            l.append(t)
+        np.save('../labels.npy', l)
+    else:
+        labels = np.load('../labels.npy')
 
-softmax_loss = mx.gluon.loss.SoftmaxCELoss(axis=1)
-lr = 0.01
-print('learning rate {}'.format(lr))
+    ctx = mx.gpu()
+    step = 32 * 8
+    train_data = mx.gluon.data.DataLoader(Isprs(images[train_idx], labels[train_idx], step, training=True),
+                                          batch_size=min(24, len(train_idx)), shuffle=True,
+                                          last_batch='rollover')
+    val_data = mx.gluon.data.DataLoader(Isprs(images[val_idx], labels[val_idx], step), batch_size=len(val_idx),
+                                        last_batch='keep')
+    net = Unet(6)
+    ckpt_name = 'ckpt_{}'.format(v)
+    if os.path.exists(ckpt_name):
+        net.load_params(ckpt_name, ctx=ctx, allow_missing=False)
+        print('load params from ckpt')
+    else:
+        net.collect_params().initialize(init=mx.init.MSRAPrelu('in'), ctx=ctx)
 
-trainer = MyTrainer(params=net.collect_params(), optimizer='sgd',
-                    optimizer_params={'learning_rate': lr, 'momentum': 0.9, 'wd': 0.0001},
-                    net=net, train_data_iter=train_data, val_data_iter=val_data, loss=softmax_loss,
-                    ckpt_name=ckpt_name, ctx=ctx, do_ckpt_epochs=100)
-trainer.train()
+    softmax_loss = mx.gluon.loss.SoftmaxCELoss(axis=1)
+    lr = 0.01
+    print('learning rate {}'.format(lr))
+
+    trainer = MyTrainer(params=net.collect_params(), optimizer='sgd',
+                        optimizer_params={'learning_rate': lr, 'momentum': 0.9, 'wd': 0.0001},
+                        net=net, train_data_iter=train_data, val_data_iter=val_data, loss=softmax_loss,
+                        ckpt_name=ckpt_name, ctx=ctx, do_ckpt_epochs=100)
+    trainer.train()
