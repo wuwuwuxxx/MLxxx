77b7a99d4f52d6b9a1861c8ec69d277a29a3ea3b
wuwuwuxxx <wuwuwuxxx@gmail.com>
2017-09-13 14:14:19 +0800

    add normlization, fix test bug

--- test_utils/splice_image.py
+++ test_utils/splice_image.py
@@ -7,7 +7,8 @@ from test_utils.predict import predict
 def isprs_pred(img, net, step, ctx):
     h, w, _ = img.shape
     predictions = np.zeros((h,w), dtype=np.uint8)
-    img = np.transpose(img, axes=(2,0,1))
+    img = np.transpose(img, axes=(2, 0, 1)).astype(np.float32)
+    img /= 255
     img = mx.nd.array(np.expand_dims(img, 0), ctx=ctx)
     for row in range(0, h, step):
         for col in range(0, w, step):
